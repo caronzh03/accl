344d3641684c8c6514d3c52cc1202c71dcb87108
Caron Zhang <caronzh03@gmail.com>
2023-11-26 19:22:09 -0800

    Update inference tests

--- classification/experiment.py
+++ classification/experiment.py
@@ -1,29 +1,34 @@
 import torch
 from transformers import BertTokenizer
+import pandas as pd
+import numpy as np
 
 from bert_model import BertClassifier
 
 
-pos_review = "a gripping movie , played with performances that are all understated and touching."
-neg_review = "a valueless kiddie paean to pro basketball underwritten by the nba."
+# sample test dataset
+test_datapath = "/media/tianlu/SSD/datasets/stanford-sentiment-treebank/test.csv"
+df = pd.read_csv(test_datapath)
+df_test = df.sample(frac=0.1)
+sents = df_test['sentence']
 
-example_texts = [pos_review, neg_review]
-
-# tokenize
+# tokenizer
 tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
-bert_input = tokenizer(example_texts, padding='max_length', max_length=512,
-  truncation=True, return_tensors="pt")
-print(f"bert_input: {bert_input}")
 
-# inference
+# BERT model
 bert_classifier = BertClassifier()
 model_path = "models/best_bert_classifier.pt"
 bert_classifier.load_state_dict(torch.load(model_path))
 bert_classifier = bert_classifier.to("cuda")
 bert_classifier.eval()
 
-input_ids = bert_input["input_ids"].squeeze(1).to("cuda")
-mask = bert_input["attention_mask"].squeeze(1).to("cuda")
-output = bert_classifier(input_ids=input_ids, mask=mask)
-print(output)
-print(output.argmax(dim=1))
+# inference
+for sent in sents:
+  bert_input = tokenizer(sent, padding='max_length', max_length=512,
+    truncation=True, return_tensors="pt")
+  input_ids = bert_input["input_ids"].squeeze(1).to("cuda")
+  mask = bert_input["attention_mask"].squeeze(1).to("cuda")
+  output = bert_classifier(input_ids=input_ids, mask=mask)
+  predicted_label = output.argmax(dim=1).item()
+  print(f"{predicted_label}: {sent}")
+
